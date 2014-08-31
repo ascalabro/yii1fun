5d71ae81d52b9a0f587646e7fc2326cf189d077f
Angelo Scalabroni <armotek5421@msn.com>
2014-08-31 19:08:30 -0400

    Statking import of csv into new MySql table StatkingSession related to user_id, if import already exists for user, old data is replaced

--- protected/modules/Tools/controllers/PokerController.php
+++ protected/modules/Tools/controllers/PokerController.php
@@ -8,34 +8,37 @@ class PokerController extends Controller {
     }
 
     public function actionStatkingImport() {
+        $model = new StatkingImportForm();
+        if (isset($_POST['StatkingImportForm'])) {
+            $model->attributes = $_POST['StatkingImportForm'];
+            $model->dbaFile = CUploadedFile::getInstance($model, 'dbaFile');
+            if ($model->validate()) {
+                $rows = array_map('str_getcsv', file($model->dbaFile->getTempName()));
+                if (StatkingSession::model()->exists('user_id = :user_id', array(":user_id" => Yii::app()->user->id))) {
+                    $model->deleteUserOldSessions();
+                    // replace old data with new database import data
+                }
+                foreach ($rows as $x => $row) {
+                    $session = new StatkingSession();
+                    $session->user_id = Yii::app()->user->id;
+                    $session->date = date('Y-m-d', strtotime($row[0]));
+                    $session->location = $row[1];
+                    $session->game = $row[2];
+                    $session->session_length = $row[3];
+                    $session->profit_loss = $row[4];
+                    $session->comments = $row[5];
+                    $session->save();
+                }
+            }
+        }
         $this->setPageTitle("Statking Database Import");
-        $this->render('statkingimport');
+        $this->render('statkingimport', array(
+            'model' => $model
+        ));
     }
 
-    // Uncomment the following methods and override them if needed
-    /*
-      public function filters()
-      {
-      // return the filter configuration for this controller, e.g.:
-      return array(
-      'inlineFilterName',
-      array(
-      'class'=>'path.to.FilterClass',
-      'propertyName'=>'propertyValue',
-      ),
-      );
-      }
+    public function actionStatkingDatabase() {
+        
+    }
 
-      public function actions()
-      {
-      // return external action classes, e.g.:
-      return array(
-      'action1'=>'path.to.ActionClass',
-      'action2'=>array(
-      'class'=>'path.to.AnotherActionClass',
-      'propertyName'=>'propertyValue',
-      ),
-      );
-      }
-     */
 }
--- protected/modules/Tools/views/poker/statkingimport.php
+++ protected/modules/Tools/views/poker/statkingimport.php
@@ -1,23 +1,29 @@
 <?php
 /* @var $this DefaultController */
 
-$this->breadcrumbs=array(
-	$this->module->id => array('/' . $this->module->id . '/Categories'),
-        'Poker'=>array('/' . $this->module->id . '/' . $this->id),
-        $this->pageTitle
+$this->breadcrumbs = array(
+    $this->module->id => array('/' . $this->module->id . '/Categories'),
+    'Poker' => array('/' . $this->module->id . '/' . $this->id),
+    $this->pageTitle
 );
 ?>
 <div class="row">
-<h1><?php echo $this->pageTitle ?></h1>
-
-<p>
-Use this tool to import a <?php echo CHtml::link('Statking','https://www.conjelco.com/statking/purchasekey.html'); ?> database which has been exported into a "CSV" document.
-</p>
-<p>
-The action belongs to the controller "<?php echo get_class($this); ?>"
-in the "<?php echo $this->module->id; ?>" module.
-</p>
-<p>
-You may customize this page by editing <tt><?php echo __FILE__; ?></tt>
-</p>
+    <h1><?php echo $this->pageTitle ?></h1>
+    <p>
+        Use this tool to import a <?php echo CHtml::link('Statking', 'https://www.conjelco.com/statking/purchasekey.html'); ?> database which has been exported into a "CSV" document.
+    </p>
+    <?php
+    $form = $this->beginWidget(
+        'CActiveForm', array(
+        'id' => 'upload-form',
+        'enableAjaxValidation' => false,
+        'htmlOptions' => array('enctype' => 'multipart/form-data'),
+            )
+    );
+    echo $form->labelEx($model, 'dbaFile');
+    echo $form->fileField($model, 'dbaFile');
+    echo $form->error($model, 'dbaFile');
+    echo CHtml::submitButton('Submit');
+    $this->endWidget();
+    ?>
 </div>
