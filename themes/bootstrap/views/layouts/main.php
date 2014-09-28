2c303915264ac1ecd5ba6810f892cbef3076f636
Angelo Scalabroni <armotek5421@msn.com>
2014-09-27 22:37:24 -0400

    added favicon icon.ico

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -4,7 +4,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="language" content="en" />
-
+		<link rel="SHORTCUT ICON" href="<?php echo Yii::app()->baseUrl; ?>/images/icon.ico">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
